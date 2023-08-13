8e705c3743c0c46c250baf0ba774298ff3a919bf
iampranabray <pranabray04@gmail.com>
2023-08-13 17:17:38 +0400

    Initial Commit

---
+++ NewsAN/NewsANApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsANApp.swift
+//  NewsAN
+//
+//  Created by Pranab Ray on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsANUITests/NewsANUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NewsANUITestsLaunchTests.swift
+//  NewsANUITests
+//
+//  Created by Pranab Ray on 13/08/23.
+//
+
+import XCTest
+
+final class NewsANUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
